08c24a128f704a195139b65b89aa7bb3755c6dec
krassowska <gromadzka.karolina@gmail.com>
2019-03-28 17:15:22 +0100

    Add date of bite to database

--- models.py
+++ models.py
@@ -1,15 +1,35 @@
 from app import app, db
-
+import datetime as dt
 
 class Tick(db.Model):
+
+    def __init__(self, **kwargs):
+
+        if not isinstance(kwargs['date'], dt.datetime):
+            date = dt.datetime.strptime(kwargs['date'], '%Y-%m-%d')
+            print(type(date))
+            kwargs['date'] = date
+
+        super().__init__(**kwargs)
+        print(kwargs['date'])
+
     id = db.Column(db.Integer, primary_key=True)
     longitude = db.Column(db.Float(), nullable=False)
     latitude = db.Column(db.Float(), nullable=False)
     is_test = db.Column(db.Boolean(), nullable=False)
-    # date_of_bite = db.Column(db.Date(), nullable=False)
+    date = db.Column(db.Date(), nullable=False)
     # disease = db.Column(db.String(100))
     sex = db.Column(db.Boolean(), nullable=False)
     age = db.Column(db.Integer, nullable=True)
+
+
+    def czy_jest_starszy_niz(self, n_lat):
+        return self.age > n_lat
+
+    @property
+    def sex_name(self):
+        return 'Female' if self.sex else 'Male'
+    # Female = True, Male = False
 
     def __repr__(self):
         return '<Tick %r>' % self.id
