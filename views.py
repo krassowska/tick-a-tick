2dc4fb5cd42fecc16dd7ea1204cbd0d695bb9fea
krassowska <gromadzka.karolina@gmail.com>
2019-03-28 17:15:49 +0100

    Add date of bite to database

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from app import app, db
+from models import Tick
+import datetime as dt
 
 x = 0
 
@@ -9,16 +11,42 @@ def index():
     x += 1
     return render_template('home.html', licznik=x)
 
-@app.route('/add_tick')
+
+@app.route('/add_tick', methods=['GET', 'POST'])
 def add_tick():
-    response = 'You added a new tick. :)'
-    return render_template('home.html', response=response)
+    if request.method == 'POST':
+        cities_locations = {'Warsaw': {'longitude': 52.22977, 'latitude': 21.01178}}
+        tick_city = request.form['location'] # Warszawa
+        tick_location = cities_locations[tick_city]
+        date = dt.datetime.strptime(request.form['date'], '%Y-%m-%d')
+        age = request.form['age']
+        new_tick = Tick(
+            longitude=tick_location['longitude'],
+            latitude=tick_location['latitude'],
+            sex=True,
+            age=age,
+            is_test=True,
+            date=date
+        )
 
+        db.session.add(new_tick)
+        db.session.commit()
 
-@app.route('/statistics')
-def statistics():
+        response = f'You added a new tick from { tick_city }, catched on { date }. You are {age} years old.'
+        return render_template('add_tick.html', response=response)
+    else:
+        response = ''
+        return render_template('add_tick.html', response=response)
+
+@app.route('/ticks')
+def ticks():
+    ticks = Tick.query.all()
+    return render_template('ticks_table.html', ticks=ticks)
+
+@app.route('/tick_statistics')
+def tick_statistics():
     x = 'usmiech'
-    return render_template('statistics.html', chart=x)
+    return render_template('tick_statistics.html', chart=x)
 
 @app.route('/knowledge')
 def knowledge():
