f532d9bd0e2eefbfe441bbab223ceb8ba6cf2d94
krassowska <gromadzka.karolina@gmail.com>
2019-04-03 18:05:13 +0200

    Conect credentials to tweets.py

--- tweets.py
+++ tweets.py
@@ -2,11 +2,18 @@ from flask import Flask, render_template, request
 from app import app
 import tweepy
 
+'''
+
 with open('credentials.txt', 'r') as file:
     consumer_key = file.readline().split()[2]
     consumer_secret = file.readline().split()[2]
     access_token = file.readline().split()[2]
     access_token_secret = file.readline().split()[2]
+'''
+consumer_key = os.environ.get('consumer_key', None)
+consumer_secret = os.environ.get('consumer_secret', None)
+access_token = os.environ.get('access_token', None)
+access_token_secret = os.environ.get('access_token_secret', None)
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
