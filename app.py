ed068a49036a0e605bc7afc8d2b13b54c64b30bb
krassowska <gromadzka.karolina@gmail.com>
2019-04-03 17:47:25 +0200

    Actualize app.py

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ticks.db'
 db = SQLAlchemy(app)
+port = int(os.environ.get('PORT', 5000))
 
 app.secret_key = 'ThisIsVerySecretKey'
 
 import views
 import models
 import tweets
+
+app.run(host='0.0.0.0', port=port, debug=True)
