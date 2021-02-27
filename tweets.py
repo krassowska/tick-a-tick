fe3ce83373c2d044efbc88061d1729a089b836a5
Karolina Krassowska <39529572+krassowska@users.noreply.github.com>
2021-02-27 23:04:18 +0000

    Fix lint errors
    ./tweets.py:1:1: F401 'flask.Flask' imported but unused
    ./tweets.py:1:1: F401 'flask.request' imported but unused
    ./tweets.py:17:1: E302 expected 2 blank lines, found 1
    ./tweets.py:19:5: E265 block comment should start with '# '
    ./tweets.py:20:5: E265 block comment should start with '# '
    ./tweets.py:24:1: E265 block comment should start with '# '
    ./tweets.py:25:1: E265 block comment should start with '# '
    ./tweets.py:29:60: W292 no newline at end of file
    ./tweets.py:29:60: W292 no newline at end of file
    ./tweets.py:29:60: W292 no newline at end of file

--- tweets.py
+++ tweets.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import render_template
 from app import app
 import tweepy
 import os
@@ -14,16 +14,8 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+
 @app.route('/news', methods=["GET"])
 def news():
-    #twitter_names =
-    #for name in twitter_names:
     tweets_from_timeline = api.user_timeline(screen_name='Lymenews', count=5)
     return render_template('home.html', tweets_from_timeline=tweets_from_timeline)
-
-#@app.route('/tweet', methods=["POST"])
-#def tweet():
-#    text = request.form['Tweet']
-#    post_tweet = api.update_status(text)
-#    response = f"Your tweet was send: {text}"
-#    return render_template('main.html', response=response)
