b59d7d09d1e90a2bb1ec8cfb2a054fec34278dde
Karolina Krassowska <39529572+krassowska@users.noreply.github.com>
2021-02-27 23:00:21 +0000

    Fix lint error

--- create_db.py
+++ create_db.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 from app import db
+from models import Tick
+
+
 db.create_all()
 
-from models import Tick
 franio = Tick(longitude=52.2297, latitude=21.0122, is_test=True, sex=False, age=18, date='2019-03-24')
 zenek = Tick(longitude=51.5074, latitude=0.1278, is_test=True, sex=False, age=17, date='2019-03-24')
 
